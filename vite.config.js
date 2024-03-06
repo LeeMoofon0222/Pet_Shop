984d28c73de9b28f039baaa5da93bcf06975dcbf
LeeMoofon0222 <moofon0222@gmail.com>
2024-03-07 01:17:32 +0800

    update pic

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Pet_Shop',
   plugins: [
     vue({
       template: { transformAssetUrls }
